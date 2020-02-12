b81cf016bfab275e81586ff031867db6e3d69256
Alumno <Alumno@PC-PC>
2020-02-12 11:33:19 -0300

    Persona actualizado

--- Clase19/src/Clase19/Persona.java
+++ Clase19/src/Clase19/Persona.java
@@ -15,6 +15,43 @@ public class Persona {
 		this.estadoCivil = estadoCivil;
 	}
 
-	
-	
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellidos=" + apellidos + ", run=" + run + ", estadoCivil="
+				+ estadoCivil + "]";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getRun() {
+		return run;
+	}
+
+	public void setRun(String run) {
+		this.run = run;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+		
 }
